88418b1eaa8444f53dac351de46f583a00a1eab5
lobatoalain <108066824+lobatoalain@users.noreply.github.com>
2023-01-08 02:28:20 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,47 @@
+
+function alteraFrase(frase){
+    const texto = document.getElementsByTagName('h1')
+
+    texto[0].innerHTML = frase
+}
+
 const sim = document.querySelector('[data-sim]')
 const nao = document.querySelector('[data-nao]')
 
-sim.addEventListener("click", function(){
-    let numAleatorio1 = parseInt(Math.random()*200)
-    let numAleatorio2 = parseInt(Math.random()*200)
+sim.addEventListener("click", ()=>{
+    let top = parseInt(Math.random()*200)
+    let right = parseInt(Math.random()*200)
 
-    console.log(numAleatorio1)
-    console.log(numAleatorio2)
+    console.log(top)
+    console.log(right)
 
-    sim.style.top = numAleatorio1+'px'
-    sim.style.right = numAleatorio2+'px'
+    sim.style.top = top+'px'
+    sim.style.right = right+'px'
 })
 
-nao.addEventListener("click", function(){
+nao.addEventListener("click", ()=>{
     alert('Resposta correta')
+})
+
+const alterar = document.querySelector('#altera')
+const caixaAltera = document.querySelector('.caixaAlteraFrase')
+const alterou = document.querySelector('#alterou')
+
+alterar.addEventListener("click",()=>{
+    alterar.classList.remove('ativa')
+    alterar.classList.add('inativa')
+
+    caixaAltera.classList.remove('inativa')
+    caixaAltera.classList.add('ativa')
+})
+
+alterou.addEventListener("click", ()=>{
+    const textoAlterado = document.querySelector('#texto').value
+    
+    alterar.classList.remove('inativa')
+    alterar.classList.add('ativa')
+    
+    caixaAltera.classList.add('inativa')
+    caixaAltera.classList.remove('ativa')
+    alteraFrase(textoAlterado)
 })
