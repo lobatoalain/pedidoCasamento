2485f16dde576d92ed9f7e704a5764e3e6a50199
lobatoalain <108066824+lobatoalain@users.noreply.github.com>
2023-01-07 23:39:33 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const sim = document.querySelector('[data-sim]')
+const nao = document.querySelector('[data-nao]')
+
+sim.addEventListener("click", function(){
+    let numAleatorio1 = parseInt(Math.random()*250)
+    let numAleatorio2 = parseInt(Math.random()*400)
+
+    console.log(numAleatorio1)
+    console.log(numAleatorio2)
+
+    sim.style.top = numAleatorio1+'px'
+    sim.style.right = numAleatorio2+'px'
+})
+
+nao.addEventListener("click", function(){
+    alert('Resposta correta')
+})
